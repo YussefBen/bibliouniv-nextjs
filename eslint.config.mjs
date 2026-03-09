6eb33edd5de3ac8f0ed21a5f4e7004d7aa0bdbf8
YussefBen <yussef.acm@gmail.com>
2026-03-09 13:58:42 +0100

    Initialisation du projet Next.js 15 avec Tailwind CSS

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn", 
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }], 
+      "react/no-unescaped-entities": "off", 
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "node_modules/**",
+  ]),
+]);
+
+export default eslintConfig;
